e28e5707cd3d974132ff2cf922f713d904e8f48f
DESKTOP-3ESP4L4\Poço Martins <gerci.filho@gmail.com>
2017-01-12 22:06:01 -0200

    Inclusão de ListView

--- app/src/main/java/br/com/pocomartins/tupicionario/MainActivity.java
+++ app/src/main/java/br/com/pocomartins/tupicionario/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.pocomartins.tupicionario;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvBichos = (TextView) findViewById(R.id.tvbichos);
+        tvBichos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, BichosActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView tvCulinaria = (TextView) findViewById(R.id.tvculinaria);
+        tvCulinaria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CulinariaActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView tvPlantas = (TextView) findViewById(R.id.tvplantas);
+        tvPlantas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PlantasActivity.class);
+                startActivity(i);
+            }
+        });
+
+        TextView tvPovos = (TextView) findViewById(R.id.tvpovos);
+        tvPovos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, PovosNativosActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
